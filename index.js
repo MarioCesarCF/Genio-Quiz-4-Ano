002d4e93552f23f6cd3120852da408a6008b1792
Mário Cesar Cunha <mc.fialho@hotmail.com>
2022-08-04 08:56:38 -0300

    update

--- index.js
+++ index.js
@@ -2,66 +2,90 @@ const largada = document.getElementById('largada');
 const tartaruga = document.getElementById('tartaruga');
 const caracol = document.getElementById('caracol');
 const estrela = document.getElementById('estrela');
-let counterTart = 0;
-let counterCarc = 0;
-let counterEstr = 0;
+
 let marginLeftTart = 0;
+let timerTart = '';
+let marginLeftCarc = 0;
+let timerCarc = '';
+let marginLeftEstr = 0;
+let timerEstr = '';
 
 largada.addEventListener('click', (e) => {
     e.preventDefault();
+    largada.disabled = true;
+
     tartaruga.style.marginLeft = '0';
     marginLeftTart = 0;
-    if(counterTart < 20) {
-        let timerTart = setInterval(() => {
-            marginLeftTart += 10;
-            console.log(counterTart);
-            tartaruga.style.marginLeft = `${marginLeftTart}px`;
-            counterTart += 1;
-            if(marginLeftTart > 250) {
-                setTimeout(() => { 
-                    clearInterval(timerTart); 
-                    alert('Tartaruga Venceu!!'); 
-                }, 1000);
-            }
-        }, 1000);
-    } else {
-        let timerTart = setInterval(() => {
-            marginLeftTart += 10;
-            console.log(marginLeftTart)
-            tartaruga.style.marginLeft = `${marginLeftTart}px`;
-            counterTart += 1;
-            if(marginLeftTart > 250) {
-                setTimeout(() => { 
-                    clearInterval(timerTart); 
-                    alert('Tartaruga Venceu!!'); 
-                }, 1000);
-            }
-        }, 1000);
-    }
-    
+    caracol.style.marginLeft = '0';
+    marginLeftCarc = 0;
+    estrela.style.marginLeft = '0';
+    marginLeftEstr = 0;
+
+    tartarugaAnda1();
+    caracolAnda1();
+    estrelaAnda1();
 })
-// function largada() {
-//     if(counterTart > 10) {
-//         setInterval(tartarugaAnda1(), 1000);
-//     } else {
-//         //tartarugaAnda2();
-//     }
-    
-//     //caracolAnda();
-//     //estrelaAnda();
-// }
 
-//setInterval(tartarugaAnda1(), 1000);
 
-// function tartarugaAnda1() {        
-//         setInterval(() => {        
-//         marginLeftTart += 10;
-//         console.log(marginLeftTart)
-//         tartaruga.style.marginLeft = `${marginLeftTart}px`;
-//         counterTart += 1;
-//         if(marginLeftTart > 250) {
-//             setTimeout(() => { clearInterval(timerTart); alert('Tartaruga Venceu!!'); }, 1000);
-//         }
-//     }, 1000);
-// }
+function tartarugaAnda1() {
+    timerTart = setInterval(() => {
+        marginLeftTart += 10;
+        tartaruga.style.marginLeft = `${marginLeftTart}px`;
+        
+        if (marginLeftTart > 120) {
+            setTimeout(() => {
+                clearInterval(timerTart);
+                timerTart = setInterval(() => {
+                    marginLeftTart += 10;
+                    tartaruga.style.marginLeft = `${marginLeftTart}px`;
+                    
+                    if (marginLeftTart > 260) {
+                        setTimeout(() => {
+                            clearInterval(timerTart);
+                            clearInterval(timerCarc);
+                            clearInterval(timerEstr);
+                            alert('Tartaruga Venceu!!');
+                        });
+                    }
+                }, 1000);
+            });
+        }
+    }, 2000);
+}
+
+function caracolAnda1() {
+    timerCarc = setInterval(() => {
+        marginLeftCarc += 10;
+        caracol.style.marginLeft = `${marginLeftCarc}px`;
+        
+        if (marginLeftCarc > 100) {
+            setTimeout(() => {
+                clearInterval(timerCarc);
+                timerCarc = setInterval(() => {
+                    marginLeftCarc += 10;
+                    caracol.style.marginLeft = `${marginLeftCarc}px`;
+                                        
+                }, 3000);
+            });
+        }
+    }, 500);
+}
+
+function estrelaAnda1() {
+    timerEstr = setInterval(() => {
+        marginLeftEstr += 10;
+        estrela.style.marginLeft = `${marginLeftEstr}px`;
+        
+        if (marginLeftEstr > 100) {
+            setTimeout(() => {
+                clearInterval(timerEstr);
+                timerEstr = setInterval(() => {
+                    marginLeftEstr += 10;
+                    estrela.style.marginLeft = `${marginLeftEstr}px`;
+                                        
+                }, 2100);
+            });
+        }
+    }, 1000);
+}
 
